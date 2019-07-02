7129d6e43baf5c146a050ae9356aed598f55c0e7
Kshitij <38787855+kshtijsingh@users.noreply.github.com>
2019-07-03 05:22:37 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+  <head>
+    <link rel="stylesheet" href="public/static/style2.css" >
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src= 'public/static/script.js'></script>
+    <link href="https://fonts.googleapis.com/css?family=Orbitron" rel="stylesheet">
+
+  </script>
+  </head>
+  <body class="containerfluid">
+
+  <div class="loader">
+  <span class="loader__inner"></span>
+  <span class="loader__inner"></span>
+
+  <div style= "text-align: center; color: white;font-size: 40px;font-family:'Orbitron', sans-serif;
+  "><h2>Q.me</h2>
+    
+
+  </div>
+</div>
+
+<div style= 'position: absolute; bottom: 0; left: 40%; height: 50%'>
+  <a href= 'public/templates/login.php' style= 'text-decoration: none'><h4 style= "text-align: center; color: white;font-size: 25px;font-family:'Orbitron', sans-serif;">Enter the world</h4></a>
+</div>
+ 
+  </body>
+</html>
